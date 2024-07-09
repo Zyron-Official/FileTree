692cc1e944cd3af06f22453f38220d97cea738aa
Sheikh Abdul Aziz <sheikhabdulaziz.ml@gmail.com>
2024-07-09 13:25:50 +0530

    Update FileTreeNode.kt

--- filetree/src/main/kotlin/com/zyron/filetree/model/FileTreeNode.kt
+++ filetree/src/main/kotlin/com/zyron/filetree/model/FileTreeNode.kt
@@ -2,4 +2,4 @@ package com.zyron.filetree.model
 
 import java.io.File
 
-data class FileTreeNode(val file: File, var isExpanded: Boolean = false, var parent: FileTreeNode? = null)
+data class FileTreeNode(val file: File, var isExpanded: Boolean = false, var isCollapsed: Boolean = false, var parent: FileTreeNode? = null, var isLoading: Boolean = false)
