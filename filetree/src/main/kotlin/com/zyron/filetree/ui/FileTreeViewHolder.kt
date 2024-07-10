65e2fada6fcaafc7cde77eb14cc16d85473c3133
Sheikh Abdul Aziz <sheikhabdulaziz.ml@gmail.com>
2024-07-10 10:47:52 +0530

    Update FileTreeViewHolder.kt

--- filetree/src/main/kotlin/com/zyron/filetree/ui/FileTreeViewHolder.kt
+++ filetree/src/main/kotlin/com/zyron/filetree/ui/FileTreeViewHolder.kt
@@ -5,13 +5,14 @@ import android.widget.ViewFlipper
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.imageview.ShapeableImageView
 import com.google.android.material.textview.MaterialTextView
+import com.google.android.material.progressindicator.CircularProgressIndicator
 
 import com.zyron.filetree.R
 
 class FileTreeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    val chevronIconViewHolder: ShapeableImageView = itemView.findViewById(R.id.chevronImageView)
-    val fileIconViewHolder: ShapeableImageView = itemView.findViewById(R.id.fileIconImageView)
-    val fileNameViewHolder: MaterialTextView = itemView.findViewById(R.id.fileNameTextView)
     val chevronLoadingSwitcher: ViewFlipper = itemView.findViewById(R.id.chevronLoadingSwitcher)
-    
+    val chevronImageView: ShapeableImageView = itemView.findViewById(R.id.chevronImageView)
+    val loadingProgressView: CircularProgressIndicator = itemView.findViewById(R.id.laodingProgressIndicator)
+    val fileIconImageView: ShapeableImageView = itemView.findViewById(R.id.fileIconImageView)
+    val fileNameTextView: MaterialTextView = itemView.findViewById(R.id.fileNameTextView)
 }
