a8a0fca2016b6db9c3d6f0b88174e713bb1f1d4a
Sheikh Abdul Aziz <sheikhabdulaziz.ml@gmail.com>
2024-08-04 21:42:46 +0530

    Update filetree

---
+++ app/src/main/kotlin/com/zyron/filetree/executorservice/FileOperationExecutor.kt
@@ -0,0 +1,27 @@
+package com.zyron.filetree.executorservice
+
+import android.content.Context
+import android.widget.Toast
+import com.zyron.filetree.adapter.FileTreeEventListener
+import java.io.File
+
+class FileOperationExecutor(private val context: Context) : FileTreeClickListener {
+
+    override fun onFileClick(file: File) {
+        Toast.makeText(context, "File clicked: ${file.name}", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onFolderClick(folder: File) {
+        Toast.makeText(context, "Folder clicked: ${folder.name}", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onFileLongClick(file: File): Boolean {
+        Toast.makeText(context, "File long-clicked: ${file.name}", Toast.LENGTH_SHORT).show()
+        return true
+    }
+
+    override fun onFolderLongClick(folder: File): Boolean {
+        Toast.makeText(context, "Folder long-clicked: ${folder.name}", Toast.LENGTH_SHORT).show()
+        return true
+    }
+}
