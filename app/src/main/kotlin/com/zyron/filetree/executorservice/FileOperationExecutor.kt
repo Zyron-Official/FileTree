9adf501fab88608dc36b56a1cd8ab2921fc91fe4
Sheikh Abdul Aziz <sheikhabdulaziz.ml@gmail.com>
2024-08-04 21:43:50 +0530

    Update FileOperationExecutor.kt

--- app/src/main/kotlin/com/zyron/filetree/executorservice/FileOperationExecutor.kt
+++ app/src/main/kotlin/com/zyron/filetree/executorservice/FileOperationExecutor.kt
@@ -5,7 +5,7 @@ import android.widget.Toast
 import com.zyron.filetree.adapter.FileTreeEventListener
 import java.io.File
 
-class FileOperationExecutor(private val context: Context) : FileTreeClickListener {
+class FileOperationExecutor(private val context: Context) : FileTreeEventListener {
 
     override fun onFileClick(file: File) {
         Toast.makeText(context, "File clicked: ${file.name}", Toast.LENGTH_SHORT).show()
